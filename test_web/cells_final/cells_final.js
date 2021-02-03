fc40267ca52f3b1a260fb28b8b56d21a199bb969
fergarundel <68985154+fergarundel@users.noreply.github.com>
2021-02-03 14:46:24 +1100

    Update cells_final.js

--- test_web/cells_final/cells_final.js
+++ test_web/cells_final/cells_final.js
@@ -32,7 +32,11 @@ function setup() {
   song.loop();
 
   for (var i = 0;i < 500;i++){
-    var vehicle = new Vehicle(random(width/2 +30,width/2 -30),random(height/2 +30,height/2 -30));
+   
+    var vehicle = new font.textToPoints('~',posx,posy,width/5,{ 
+     sampleFactor: 0.3
+  });
+  //  var vehicle = new Vehicle(random(width/2 +30,width/2 -30),random(height/2 +30,height/2 -30));
     vehicles.push(vehicle);    
   } 
 }
