0c4e37e57d2a8727c9571dac76fc0eae4a5db115
fergarundel <fergus.j.arundel@gmail.com>
2021-03-11 16:33:52 +1100

    sketch

--- loading_screen_mobile/sketch.js
+++ loading_screen_mobile/sketch.js
@@ -12,7 +12,7 @@ let timeoutHandle = null;
 let intervalHandle2 = null;
 let growing = true;
 
-let text = ['EXPERIMENTAL','EXPERIMENTAL / MULTI-SENSORY','EXPERIMENTAL / MULTI-SENSORY / IMMERSIVE','REELIZE.STUDIO'];
+let text = [''];
 index = 0;
 let wordcount= 0;
 
@@ -30,11 +30,11 @@ function setup() {
   bass.setVolume(0.35);
   song.loop();
 
-  let bounds2= font.textBounds('~', 0, 0, width/9);
+  let bounds2= font.textBounds('~', 0, 0, width/2);
   let posx2 = width / 2 - bounds2.w / 2;
   let posy2 = height / 2 + bounds2.h / 2;
 
-  let cells = font.textToPoints('~',posx2,posy2,width/9,{ 
+  let cells = font.textToPoints('~',posx2,posy2,width/2,{ 
     sampleFactor: .3,
   });
 
@@ -48,7 +48,6 @@ for (var i = 0;i < cells.length;i++){
 
 function draw() {
   background (0);
-  print (wordcount);
 
   for (var i =0; i < vehicles.length ;i++){
     var vehicle = vehicles [i];
@@ -79,89 +78,24 @@ function pulse () {
     growing = true;
   }
 }
-
- function cycleText (){
-   intervalHandle = setInterval(loopingText,2000);
- }
   
- function mousePressed(){  
-  timeoutHandle = setTimeout(cycleText,0);
-   
+ function mousePressed(){     
    if (on == false){
-   timeoutHandle;
    bass.play();
    wave.play();
-  //  clearInterval (intervalPulse);
 
    for (let i = 0; i < vehicles.length; i++){
-    vehicles[i].target.x = random(width);
-    vehicles[i].target.y = random(height);
+    vehicles[i].target.x = random(width)+ random (-100000,100000);
+    vehicles[i].target.y = random(height)+ random (-100000,100000);
       }
    }
 
    if (on == true){
-    bass.play();
     wave.play();
-    wordcount = text.length + 5;
-
-      for (let i = 0; i < vehicles.length; i++){
-        vehicles[i].target.x = width/2 + random (-100000,100000);
-        vehicles[i].target.y = height/2 + random (-100000,100000);
-         }
-
-      clearInterval(intervalHandle);
-      index = text.length + 1;
    }
     on = true;
     pulseStop = true;
  }
-
-function loopingText (){
-  wordcount ++;
-
-  let bounds = font.textBounds(text[index], 0, 0, width/25);
-  let posx = width / 2 - bounds.w / 2;
-  let posy = height / 2 + bounds.h / 2;
-
-  let cells = font.textToPoints(text[index],posx,posy,width/25,{ 
-     sampleFactor: 0.3
-  });
-    
-     if (index < text.length){
-   if (cells.length < vehicles.length){
-   vehicles.splice(cells.length -1, vehicles.length - cells.length);
-   bass.play();
-  
-  for (let i = 0;i < cells.length; i++){
-    vehicles[i].target.x = cells[i].x;
-    vehicles[i].target.y = cells[i].y; 
-    }
-  }
-  
-  else if (cells.length > vehicles.length){
-     bass.play();
-      for (let i = vehicles.length; i< cells.length;i++){
-      let v = vehicles[i - vehicles.length].clone();
-      vehicles.push(v);
-    }
-    
-    for (let i=0; i<cells.length;i++){
-      vehicles[i].target.x = cells[i].x;
-      vehicles[i].target.y = cells[i].y;
-    } 
-  } else {
-    for (let i = 0; i < cells.length; i++){
-      vehicles[i].target.x = cells[i].x;
-      vehicles[i].target.y = cells[i].y;
-    }
-  }
- } 
-    
- index ++; 
-//  if (index > text.length - 1){
-//    index = 0;
-//   }
-}
 
 class Vehicle {
   constructor(x,y){
@@ -169,8 +103,8 @@ class Vehicle {
     this.target = createVector(x,y);
     this.vel = createVector();
     this.acc = createVector();
-    this.maxspeed = 13;
-    this.maxforce = 1.4;
+    this.maxspeed = 11;
+    this.maxforce = 0.8;
   }
  
   behaviours(){
@@ -207,7 +141,7 @@ class Vehicle {
 
     stroke (255);
     strokeWeight(size);
-    point(this.pos.x+random(-1,1),this.pos.y+random(-1,1));
+    point(this.pos.x+random(-1,1) ,this.pos.y+random(-1,1));
 }
 
   clone () {
